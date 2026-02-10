d6046f1bf26e39d3c24be4910530abae68b6586e
sruthi211020 <sujithasruthi2000@gmail.com>
2026-02-10 20:02:27 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div style={{background:"#eee", padding:"10px", marginTop:"20px"}}>
+      <p>© 2026 Dress Shop</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div style={{background:"#f06292", padding:"15px", color:"white"}}>
+      <h2>Dress Shop</h2>
+    </div>
+  );
+}
+
+export default Header;
